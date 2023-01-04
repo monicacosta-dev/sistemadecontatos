866d6f07bb96f1d4003307de85f686d9d3df0127
Monica Costa <monicacosta07@gmail.com>
2023-01-04 11:09:30 -0300

    Pasta Repositorio adicionada juntamente com os arquivos de controle e interface

---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,63 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext; //criando uma variavel
+
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            this._bancoContext= bancoContext;
+        }
+
+        public ContatoModel ListarPorId(int id)
+        {
+            return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ContatoModel> BuscarTotos()
+        {
+            return  _bancoContext.Contatos.ToList();
+        }
+
+        public ContatoModel Acicionar(ContatoModel contato)
+        {
+            // gravar dados no Banco de Dados]
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges(); //commitar - confirma
+            return contato;
+        }
+
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if (contatoDB == null) throw new System.Exception("OH!Houve um erro na atualização do contato!!!");
+
+            contatoDB.Name= contato.Name;
+            contatoDB.Email= contato.Email;
+            contatoDB.Celular = contato.Celular;
+
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel contatoDB = ListarPorId(id);
+
+            if (contatoDB == null) throw new System.Exception("OH!Houve um erro na deleção do contato!!!");
+
+            _bancoContext.Contatos.Remove(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
---
+++ ControleDeContatos/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel ListarPorId(int id); //contrato da interface - Metodo
+
+        List<ContatoModel> BuscarTotos();//metodo de buscar todos os clientes
+
+        ContatoModel Acicionar(ContatoModel contato); //criando metodo
+
+        ContatoModel Atualizar(ContatoModel contato);
+
+        bool Apagar(int id);
+    }
+}
