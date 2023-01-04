48672473c7111c62434655dcdda7db39cdef63f1
Monica Costa <monicacosta07@gmail.com>
2023-01-04 10:10:42 -0300

    Arquivos da model adicionado - pasta Models

---
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; } //gera automaticamente
+        [Required(ErrorMessage ="Nome do contato, é obrigatorio!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "E-mail do contato é obrigatorio!")]
+        [EmailAddress(ErrorMessage ="E-mail digitado não é valido!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Celular do contato é obrigatorio!")]
+        [Phone(ErrorMessage ="Celular informado não é valido!")]
+        public string Celular { get; set; }
+
+    }
+}
